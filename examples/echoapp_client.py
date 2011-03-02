8c6452006f0dfc4a812ae451ccc6bd883faa1c98
liris <liris.pp@gmail.com>
2011-03-02 21:21:49 +0900

    changed the echo server URL

--- examples/echoapp_client.py
+++ examples/echoapp_client.py
@@ -24,7 +24,7 @@ def on_open(ws):
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://localhost:5000/chat",
+    ws = websocket.WebSocketApp("ws://echo.websocket.org/",
                                 on_message = on_message,
                                 on_error = on_error,
                                 on_close = on_close)
