e6c20ecd23cd2d87e6d3086007e668632904c542
liris <liris.pp@gmail.com>
2014-03-20 09:39:24 +0900

    - remove some comment

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         "Intended Audience :: Developers",
     ],
     keywords='websockets',
-#    py_modules=["websocket"],
     scripts=["bin/wsdump.py"],
     install_requires=['backports.ssl_match_hostname'],
     packages=["tests", "websocket"],
