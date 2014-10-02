bc0c43eece25a60f5b93638c5159e81fde727312
Eran Keydar <ekeydar@gmail.com>
2014-10-03 00:12:16 +0300

    code and reason for close callback

--- websocket/_app.py
+++ websocket/_app.py
@@ -16,7 +16,7 @@ Copyright (C) 2010 Hiroki Ohtani(liris)
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
->
+
 """
 
 """
