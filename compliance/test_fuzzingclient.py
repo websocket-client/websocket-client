d18abf30838dd03e7cd74b77826ad568c1e7c6f0
Hiroki Ohtani <liris.pp@gmail.com>
2014-10-14 11:42:56 +0900

    refs #117   my first trial commit of Authbahn test suite.

---
+++ compliance/test_fuzzingclient.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import websocket
+import json
+import traceback
+
+SERVER = 'ws://127.0.0.1:8642'
+AGENT = 'py-websockets-cleint'
+
+
+ws = websocket.create_connection(SERVER + "/getCaseCount")
+count = json.loads(ws.recv())
+ws.close()
+
+success = 0
+failed = 0
+
+for case in range(1, count+1):
+	url = SERVER + '/runCase?case={}&agent={}'.format(case, AGENT)
+	try:
+		ws = websocket.create_connection(url)
+		opcode, msg = ws.recv_data()
+		if opcode  in (websocket.ABNF.OPCODE_TEXT, websocket.ABNF.OPCODE_BINARY):
+			ws.send(msg, opcode)
+		success += 1
+	except Exception as e:
+		failed += 1
+		print("[Faield] Test Case: " + str(case))
+		print(traceback.format_exc())
+	finally:
+		ws.close()
+
+print("Ran {} test cases. success: {}, faield: {}".format(case, success, failed))
+url = SERVER + '/updateReports?agent={}'.format(AGENT)
+ws = websocket.create_connection(url)
