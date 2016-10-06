2f8fbf99d3a0c4980144ab5fee2e8f915467d439
liris <liris.pp@gmail.com>
2016-10-06 09:47:40 +0900

    cannot use long literal on python3

--- websocket/_url.py
+++ websocket/_url.py
@@ -96,7 +96,7 @@ def _is_subnet_address(hostname):
 def _is_address_in_network(ip, net):
     ipaddr = struct.unpack('I', socket.inet_aton(ip))[0]
     netaddr, bits = net.split('/')
-    netmask = struct.unpack('I', socket.inet_aton(netaddr))[0] & ((2L << int(bits) - 1) - 1)
+    netmask = struct.unpack('I', socket.inet_aton(netaddr))[0] & ((2 << int(bits) - 1) - 1)
     return ipaddr & netmask == netmask
 
 
