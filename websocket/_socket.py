33295adf29ef9f62790393a2060362eb4182e0c6
Hiroki Ohtani <liris.pp@gmail.com>
2018-01-22 20:24:26 +0900

    revert

--- websocket/_socket.py
+++ websocket/_socket.py
@@ -35,7 +35,7 @@ if hasattr(socket, "TCP_KEEPIDLE"):
     DEFAULT_SOCKET_OPTION.append((socket.SOL_TCP, socket.TCP_KEEPIDLE, 30))
 if hasattr(socket, "TCP_KEEPINTVL"):
     DEFAULT_SOCKET_OPTION.append((socket.SOL_TCP, socket.TCP_KEEPINTVL, 10))
-if hasattr(socket, "TCP_KEEPCNT") and sys.platform != 'win32':
+if hasattr(socket, "TCP_KEEPCNT"):
     DEFAULT_SOCKET_OPTION.append((socket.SOL_TCP, socket.TCP_KEEPCNT, 3))
 
 _default_timeout = None
