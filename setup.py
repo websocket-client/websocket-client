834e63c69224dc240a558f4a26ddb5b7e8a299f4
Hugo <hugovk@users.noreply.github.com>
2018-06-17 18:50:03 +0300

    Add support for Python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
