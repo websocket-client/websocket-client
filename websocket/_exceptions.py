e2db34d94e5566a95ac2747502b831fbce0712c6
tarma <tarma_fu@hotmail.com>
2018-11-05 12:05:18 +0800

    Add response header in WebSocketBadStatusException

--- websocket/_exceptions.py
+++ websocket/_exceptions.py
@@ -74,10 +74,11 @@ class WebSocketBadStatusException(WebSocketException):
     WebSocketBadStatusException will be raised when we get bad handshake status code.
     """
 
-    def __init__(self, message, status_code, status_message=None):
+    def __init__(self, message, status_code, status_message=None, resp_headers=None):
         msg = message % (status_code, status_message)
         super(WebSocketBadStatusException, self).__init__(msg)
         self.status_code = status_code
+        self.resp_headers = resp_headers
 
 class WebSocketAddressException(WebSocketException):
     """
