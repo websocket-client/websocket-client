websocket-client/websocket-client

commit 0598ef1a7a9d72670bf592439f1899412e790989

added support for allowing users to change connection header

websocket/_handshake.py
@@ -95,14 +95,13 @@ def _pack_hostname(hostname):
 def _get_handshake_headers(resource, host, port, options):
     headers = [
         "GET %s HTTP/1.1" % resource,
-        "Upgrade: websocket",
-        "Connection: Upgrade"
+        "Upgrade: websocket"
     ]
     if port == 80 or port == 443:
         hostport = _pack_hostname(host)
     else:
         hostport = "%s:%d" % (_pack_hostname(host), port)
-
+    
     if "host" in options and options["host"] is not None:
         headers.append("Host: %s" % options["host"])
     else:
@@ -125,6 +124,11 @@ def _get_handshake_headers(resource, host, port, options):
 
     if not 'header' in options or 'Sec-WebSocket-Version' not in options['header']:
         headers.append("Sec-WebSocket-Version: %s" % VERSION)
+
+    if not 'connection' in options:
+        headers.append('Connection: upgrade')
+    else:
+        headers.append(options['connection'])
 
     subprotocols = options.get("subprotocols")
     if subprotocols:

commit 671127a2c6d007f01a862b24d865aadb80d97a7c

Fix typo in connection upgrade header.
See https://tools.ietf.org/html/rfc2817#section-3. Typo was introduced in commit 0598ef1a7a9d72670bf592439f1899412e790989.

websocket/_handshake.py
@@ -127,7 +127,7 @@ def _get_handshake_headers(resource, host, port, options):
         headers.append("Sec-WebSocket-Version: %s" % VERSION)
 
     if not 'connection' in options or options['connection'] is None:
-        headers.append('Connection: upgrade')
+        headers.append('Connection: Upgrade')
     else:
         headers.append(options['connection'])
 