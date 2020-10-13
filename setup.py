9089d832f5a5a05c9f7ff55ed486e63113a471e4
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-10-13 09:40:18 +0300

    Python 3.9 is out

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
