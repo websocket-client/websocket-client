86241b9d5d3a8c1b8dc1a5e32ea51ef73b4bd7ed
engn33r <engn33r@users.noreply.github.com>
2021-02-22 22:06:01 -0500

    Fix #526 by reverting invalid BSD license migration in commit e94ed9e to return to LGPL2.1

--- websocket/_ssl_compat.py
+++ websocket/_ssl_compat.py
@@ -15,8 +15,7 @@ Copyright (C) 2010 Hiroki Ohtani(liris)
 
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1335  USA
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 """
 __all__ = ["HAVE_SSL", "ssl", "SSLError", "SSLWantReadError", "SSLWantWriteError"]
