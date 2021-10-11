62365e985d6688055635712ffb884e5565f78f36
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-10-11 20:26:38 +0300

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
