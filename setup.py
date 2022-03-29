c2625f99789ad71f5295b86a1d2e2217ee2c5886
engn33r <engn33r@users.noreply.github.com>
2022-03-29 00:00:00 +0000

    1.3.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = "1.3.1"
+VERSION = "1.3.2"
 
 install_requires = []
 tests_require = []
--- websocket/__init__.py
+++ websocket/__init__.py
@@ -23,4 +23,4 @@ from ._exceptions import *
 from ._logging import *
 from ._socket import *
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
