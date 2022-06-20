61171591b08ee031e02cc6cb129952259062f502
engn33r <engn33r@users.noreply.github.com>
2022-06-20 00:00:00 +0000

    1.3.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ copyright = '2022'
 author = 'liris'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.2'
+release = '1.3.3'
 
 
 # -- General configuration ---------------------------------------------------
