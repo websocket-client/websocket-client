37429a3efa1238cb5b6e5c056dd4e9190575d5ca
engn33r <engn33r@users.noreply.github.com>
2022-09-04 00:00:00 +0000

    1.4.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ copyright = '2022'
 author = 'liris'
 
 # The full version, including alpha/beta/rc tags
-release = '1.4.0'
+release = '1.4.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = "1.4.0"
+VERSION = "1.4.1"
 
 install_requires = []
 tests_require = []
