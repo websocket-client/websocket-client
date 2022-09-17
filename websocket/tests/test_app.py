8dbeb2ad62ab8ef480dbfd6496936e62fe155001
Jan Schneider <j.schneider@uib.de>
2022-09-17 08:18:50 +0200

    Also check passed app in testCallbackException

--- websocket/tests/test_app.py
+++ websocket/tests/test_app.py
@@ -233,11 +233,14 @@ class WebSocketAppTest(unittest.TestCase):
         """ Test callback exception handling """
 
         exc = None
+        passed_app = None
 
         def on_open(app):
             raise RuntimeError("Callback failed")
 
         def on_error(app, err):
+            nonlocal passed_app
+            passed_app = app
             nonlocal exc
             exc = err
 
@@ -247,6 +250,7 @@ class WebSocketAppTest(unittest.TestCase):
         app = ws.WebSocketApp('ws://127.0.0.1:' + LOCAL_WS_SERVER_PORT, on_open=on_open, on_error=on_error, on_pong=on_pong)
         app.run_forever(ping_interval=2, ping_timeout=1)
 
+        self.assertEqual(passed_app, app)
         self.assertIsInstance(exc, RuntimeError)
         self.assertEqual(str(exc), "Callback failed")
 
