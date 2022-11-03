c8c5a19da2a2015a64d08479e74779de7fd355de
engn33r <engn33r@users.noreply.github.com>
2022-11-03 00:00:00 +0000

    1.4.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ copyright = '2022'
 author = 'liris'
 
 # The full version, including alpha/beta/rc tags
-release = '1.4.1'
+release = '1.4.2'
 
 
 # -- General configuration ---------------------------------------------------
