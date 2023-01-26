0895e5ce47d6b1a52f86ba24292a310d67902786
engn33r <engn33r@users.noreply.github.com>
2023-01-26 00:00:00 +0000

    Remove incorrect comment

--- websocket/tests/test_app.py
+++ websocket/tests/test_app.py
@@ -191,7 +191,6 @@ class WebSocketAppTest(unittest.TestCase):
     # def testOpcodeBinary(self):
     #     """ Test WebSocketApp binary opcode
     #     """
-    #     # The lack of wss:// in the URL below is on purpose
     #     app = ws.WebSocketApp('wss://streaming.vn.teslamotors.com/streaming/')
     #     app.run_forever(ping_interval=2, ping_timeout=1, ping_payload="Ping payload")
 
