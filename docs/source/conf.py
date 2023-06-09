99f60d4e2b8ec858f6f051a905c6c17ecb872d3b
engn33r <engn33r@users.noreply.github.com>
2023-06-09 00:00:00 +0000

    1.5.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ copyright = '2022'
 author = 'liris'
 
 # The full version, including alpha/beta/rc tags
-release = '1.5.2'
+release = '1.5.3'
 
 
 # -- General configuration ---------------------------------------------------
