b42e8b345cfa20389f6ef823ee4be291b86920f6
engn33r <engn33r@users.noreply.github.com>
2023-08-22 00:00:00 +0000

    1.6.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ copyright = '2023'
 author = 'liris'
 
 # The full version, including alpha/beta/rc tags
-release = '1.6.1'
+release = '1.6.2'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = "1.6.1"
+VERSION = "1.6.2"
 
 install_requires = []
 tests_require = []
