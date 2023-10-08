39a6876001d1170f0e1fc2754860a4697fe9b506
engn33r <engn33r@users.noreply.github.com>
2023-10-08 00:00:00 +0000

    1.6.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = "1.6.3"
+VERSION = "1.6.4"
 
 install_requires = []
 tests_require = []
