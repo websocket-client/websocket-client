5d617b6a63bfcb65b96a477f22042fb0a4b4449d
engn33r <engn33r@users.noreply.github.com>
2023-12-03 00:00:00 +0000

    update build and install processes

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-import sys
-
-import pkg_resources
 from setuptools import find_packages, setup
 
 """
--- upload.sh
+++ upload.sh
@@ -6,10 +6,10 @@ python3 -m venv /tmp/ws-venv/
 source /tmp/ws-venv/bin/activate
 
 # Install dependencies
-pip3 install -U setuptools wheel twine
+pip3 install -U twine
 
 # build the package
-python3 setup.py sdist bdist_wheel
+python -m build
 
 # Run Twine check to verify descriptions are valid
 twine check dist/*
