61bc3ebbf866aaa2f8176c49d9902125329b3a6a
engn33r <engn33r@users.noreply.github.com>
2023-12-15 05:53:51 +0000

    Make black formatter happy

--- websocket/_ssl_compat.py
+++ websocket/_ssl_compat.py
@@ -16,8 +16,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-__all__ = ["HAVE_SSL", "ssl", "SSLError", "SSLEOFError",
-    "SSLWantReadError", "SSLWantWriteError"]
+__all__ = [
+    "HAVE_SSL",
+    "ssl",
+    "SSLError",
+    "SSLEOFError",
+    "SSLWantReadError",
+    "SSLWantWriteError",
+]
 
 try:
     import ssl
