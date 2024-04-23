3dc494588c4bc6ca1cbb745e9d110108f6bf04f0
engn33r <engn33r@users.noreply.github.com>
2024-04-23 00:00:00 +0000

    1.8.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = "1.7.0"
+VERSION = "1.8.0"
 
 install_requires = []
 tests_require = []
