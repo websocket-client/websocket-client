6861ee417b65c183a81671bc57cace26c9c7ee29
Jan-Philipp Litza <jplitza@users.noreply.github.com>
2025-10-04 23:06:07 +0200

    Use ipaddress module for no_proxy parsing (#1033)
    * Use ipaddress module for no_proxy parsing
    
    This enables support for IPv6 CIDRs in the no_proxy option
    
    * Apply black formatting

--- websocket/_url.py
+++ websocket/_url.py
@@ -1,6 +1,5 @@
+import ipaddress
 import os
-import socket
-import struct
 from typing import Optional
 from urllib.parse import unquote, urlparse
 from ._exceptions import WebSocketProxyException
@@ -74,9 +73,11 @@ def parse_url(url: str) -> tuple:
 
 
 def _is_ip_address(addr: str) -> bool:
+    if not isinstance(addr, str):
+        raise TypeError("_is_ip_address() argument 1 must be str")
     try:
-        socket.inet_aton(addr)
-    except socket.error:
+        ipaddress.ip_address(addr)
+    except ValueError:
         return False
     else:
         return True
@@ -84,19 +85,18 @@ def _is_ip_address(addr: str) -> bool:
 
 def _is_subnet_address(hostname: str) -> bool:
     try:
-        addr, netmask = hostname.split("/")
-        return _is_ip_address(addr) and 0 <= int(netmask) < 32
+        ipaddress.ip_network(hostname)
     except ValueError:
         return False
+    else:
+        return True
 
 
 def _is_address_in_network(ip: str, net: str) -> bool:
-    ipaddr: int = struct.unpack("!I", socket.inet_aton(ip))[0]
-    netaddr, netmask = net.split("/")
-    netaddr: int = struct.unpack("!I", socket.inet_aton(netaddr))[0]
-
-    netmask = (0xFFFFFFFF << (32 - int(netmask))) & 0xFFFFFFFF
-    return ipaddr & netmask == netaddr
+    try:
+        return ipaddress.ip_network(ip).subnet_of(ipaddress.ip_network(net))
+    except TypeError:
+        return False
 
 
 def _is_no_proxy_host(hostname: str, no_proxy: Optional[list[str]]) -> bool:
