2814d8397e1eff7dba9fe69a46076fdd01b9d460
engn33r <engn33r@users.noreply.github.com>
2025-11-09 00:00:00 +0000

    Remove Python 3.9, EOL

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     license="Apache-2.0",
     url="https://github.com/websocket-client/websocket-client.git",
     download_url="https://github.com/websocket-client/websocket-client/releases",
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     extras_require={
         "test": ["pytest", "websockets"],
         "optional": ["python-socks", "wsaccel"],
@@ -47,7 +47,6 @@ setup(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
